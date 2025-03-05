569deb40b3f83735f95913d73800b9d51092a44b
zjp <zjp6049@gmail.com>
2025-03-05 16:47:04 +0800

    feat: 更新脚手架功能

---
+++ src/command/update.ts
@@ -0,0 +1,24 @@
+import process from "child_process";
+import chalk from "chalk";
+import ora from "ora";
+
+const spinner = ora({
+  text: "dawei-cli 正在更新....",
+  spinner: {
+    interval: 300,
+    frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"].map((item) =>
+      chalk.blue(item)
+    ),
+  },
+});
+export function update() {
+  spinner.start();
+  process.exec("npm install zjp-vite-cli@latest -g", (error) => {
+    spinner.stop();
+    if (!error) {
+      console.log(chalk.green("更新成功"));
+    } else {
+      console.log(chalk.red(error));
+    }
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { Command } from "commander";
 import { version } from "../package.json";
 import { create } from "./command/create";
-
+import { update } from "./command/update";
 const program = new Command("zjp");
+program.version(version, "-v --version");
 
-program.version(version, "-v, --version");
+program
+  .command("update")
+  .description("更新脚手架 zjp-vite-cli")
+  .action(async () => {
+    await update();
+  });
+
 program
   .command("create")
-  .description("创建一个新项")
+  .description("创建一个新项目")
   .argument("[name]", "项目名称")
   .action(async (dirName) => {
     create(dirName);
   });
+
 program.parse();
